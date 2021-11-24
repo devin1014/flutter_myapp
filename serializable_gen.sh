0f82645efe221ac0aaaa3a9db7c9a0a20616dc9d
LIUWEI <wei.liu@neulion.com.cn>
2021-11-24 18:53:45 +0800

    [update] add note

--- serializable_gen.sh
+++ serializable_gen.sh
@@ -1,3 +1,4 @@
+#flutter packages pub run build_runner clean
 flutter packages pub run build_runner build
-#when conflict add these params:--delete-conflicting-outputs
+#flutter packages pub run build_runner build --delete-conflicting-outputs
 #flutter packages pub run build_runner watch
